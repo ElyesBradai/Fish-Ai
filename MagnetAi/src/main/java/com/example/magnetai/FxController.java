3fd4e766b4f4390b374dd4316decd2a586521e1f
Ziad-Agha <123512724+Ziad-Agha@users.noreply.github.com>
2024-04-06 17:43:00 -0400

    the charge selection finally works - Ziad
    the charge selector assigns a charge to the grid and it works as a single charge selector, i made sure to remove any previous charges if a new charge was to be added

--- MagnetAi/src/main/java/com/example/magnetai/FxController.java
+++ MagnetAi/src/main/java/com/example/magnetai/FxController.java
@@ -4,12 +4,8 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -18,94 +14,136 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 
-public class FxController {
-
-
-    @FXML
-    public Button saveButton;
-    @FXML
-    public Button startButton;
-    @FXML
-    public Button resetButton;
-    @FXML
-    public Button pauseButton;
-    @FXML
-    public TextField velocityTextFieldX;
-    @FXML
-    public TextField velocityTextFieldY;
-    @FXML
-    public Slider velocitySliderX;
-    @FXML
-    public Slider velocitySliderY;
-
-    @FXML
-    private Label welcomeText;
-    @FXML
-    private FlowPane simDisplayPane;
-    @FXML
-    private Circle chargeSelector;
-    @FXML
-    private Rectangle obstacleSelector;
-    @FXML
-    private Rectangle fLSelector;
-    @FXML
-    private Rectangle sCSelector;
-
-    @FXML
-    public void initialize() {
-
-        simDisplayPane.setAlignment(Pos.CENTER);
-        createDisplay();
-
-
-    }
-
-    public void createDisplay() {
-        HBox root = new HBox();
-        root.setSpacing(Simulation.GRID_SIZE_X * Simulation.SQUARE_SIZE);
-        simDisplayPane.getChildren().add(root);
-        VBox vb1 = new VBox();
-        root.getChildren().addAll(vb1);
-        SimulationDisplay s1 = new SimulationDisplay();
-        vb1.getChildren().add(s1.getSimPane());
-        Charge c1 = new Charge(1, ChargeType.POSITIVE);
-        s1.addToMap(c1, 6);
-        Simulation s2 = new Simulation();
-
-        // Create an object property to hold the selected rectangle
-        ObjectProperty<Shape> selectedShape = new SimpleObjectProperty<>();
-        Circle selected = new Circle(50);
-        selected.setStrokeWidth(3);
-        // Bind the fill property of the circle to the fill property of the selected rectangle
-        selected.fillProperty().bind(Bindings.createObjectBinding(() ->
-                selectedShape.get() != null ? selectedShape.get().getFill() : Color.BLACK, selectedShape));
-
-
-        chargeSelector.setOnMouseClicked(event -> {
-            s1.setSelectedComponentType(Charge.TYPE);
-            System.out.println(Charge.TYPE);
-            selectedShape.set(chargeSelector);
-        });
-        obstacleSelector.setOnMouseClicked(event -> {
-            s1.setSelectedComponentType(MagneticField.TYPE);
-            System.out.println(Obstacle.TYPE);
-            selectedShape.set(obstacleSelector);
-        });
-        fLSelector.setOnMouseClicked(event -> {
-            s1.setSelectedComponentType(FinishLine.TYPE);
-            System.out.println(FinishLine.TYPE);
-            selectedShape.set(fLSelector);
-        });
-        sCSelector.setOnMouseClicked(event -> {
-            s1.setSelectedComponentType(Superconductor.TYPE);
-            System.out.println(Superconductor.TYPE);
-            selectedShape.set(sCSelector);
-        });
-
-        startButton.setOnAction(event -> {
-            s1.saveDesign();
-            s1.showAllSimulations();
-        });
+public class FxController{
+	
+	
+	public static String polarity = null;
+	@FXML
+	public ChoiceBox chargeChoiceBox;
+	@FXML
+	public Button saveButton;
+	@FXML
+	public Button startButton;
+	@FXML
+	public Button resetButton;
+	@FXML
+	public Button pauseButton;
+	@FXML
+	public TextField velocityTextFieldX;
+	@FXML
+	public TextField velocityTextFieldY;
+	@FXML
+	public Slider velocitySliderX;
+	@FXML
+	public Slider velocitySliderY;
+	@FXML
+	public TextField inwardTextField;
+	@FXML
+	public TextField outwardTextField;
+	@FXML
+	public TextField mutationRateTextField;
+	@FXML
+	public TextField inputLayersTextField;
+	@FXML
+	public TextField simulationsTextField;
+	@FXML
+	public TextField generationCounterTextField;
+	@FXML
+	public TextField strengthTextField;
+	@FXML
+	public Slider strengthSlider;
+	@FXML
+	public MenuBar menuBar;
+	@FXML
+	public BorderPane borderPane;
+	Charge c1;
+	@FXML
+	private Label welcomeText;
+	@FXML
+	private ScrollPane simDisplayPane;
+	@FXML
+	private Circle chargeSelector;
+	@FXML
+	private Rectangle obstacleSelector;
+	@FXML
+	private Rectangle fLSelector;
+	@FXML
+	private Rectangle sCSelector;
+	
+	@FXML
+	public void initialize(){
+		
+		handle();
+		createDisplay();
+		
+		
+	}
+	
+	public void handle(){
+		chargeChoiceBox.getItems().addAll("Proton", "Electron");
+		chargeChoiceBox.setOnAction(actionEvent -> {
+			polarity = (chargeChoiceBox.getValue() == null) ? null : chargeChoiceBox.getValue().toString();
+		});
+		velocitySliderX.valueProperty().addListener((observableValue, newValue, OldValue) -> {
+			Charge.velocity[0] = newValue.doubleValue();
+			velocityTextFieldX.setText(newValue.toString());
+		});
+		velocitySliderY.valueProperty().addListener((observableValue, newValue, OldValue) -> {
+			Charge.velocity[1] = newValue.doubleValue();
+			velocityTextFieldY.setText(newValue.toString());
+		});
+	}
+	
+	public void createDisplay(){
+		HBox root = new HBox();
+		root.setSpacing(Simulation.GRID_SIZE_X * Simulation.SQUARE_SIZE);
+		simDisplayPane.setContent(root);
+		VBox vb1 = new VBox();
+		root.getChildren().addAll(vb1);
+		SimulationDisplay s1 = new SimulationDisplay();
+		vb1.getChildren().add(s1.getSimPane());
+		//	Charge c1 = new Charge(ChargeType.NEGATIVE);
+		//	s1.addToMap(c1, 5);
+		for (int i = 0; i < 5; i++){
+			Simulation s2 = new Simulation();
+			
+		}
+		
+		
+		// Create an object property to hold the selected rectangle
+		ObjectProperty<Shape> selectedShape = new SimpleObjectProperty<>();
+		Circle selected = new Circle(50);
+		selected.setStrokeWidth(3);
+		// Bind the fill property of the circle to the fill property of the selected rectangle
+		selected.fillProperty().bind(Bindings.createObjectBinding(() ->
+				selectedShape.get() != null ? selectedShape.get().getFill() : Color.BLACK, selectedShape));
+		
+		
+		chargeSelector.setOnMouseClicked(event -> {
+			s1.setSelectedComponentType(Charge.TYPE);
+			System.out.println(Charge.TYPE);
+			selectedShape.set(chargeSelector);
+		});
+		obstacleSelector.setOnMouseClicked(event -> {
+			s1.setSelectedComponentType(MagneticField.TYPE);
+			System.out.println(Obstacle.TYPE);
+			selectedShape.set(obstacleSelector);
+		});
+		fLSelector.setOnMouseClicked(event -> {
+			s1.setSelectedComponentType(FinishLine.TYPE);
+			System.out.println(FinishLine.TYPE);
+			selectedShape.set(fLSelector);
+		});
+		sCSelector.setOnMouseClicked(event -> {
+			s1.setSelectedComponentType(Superconductor.TYPE);
+			System.out.println(Superconductor.TYPE);
+			selectedShape.set(sCSelector);
+		});
+		
+		startButton.setOnAction(event -> {
+			s1.saveDesign();
+			s1.showAllSimulations();
+		});
 //        VBox selectorBox = new VBox(ObstacleSelector,FLSelector,SCSelector,selected,b1);
 //        root.getChildren().add(selectorBox);
 //        Scene scene = new Scene(root, 1200, 800);
@@ -113,12 +151,12 @@ public class FxController {
 //        stage.setTitle("Hello!");
 //        stage.setScene(scene);
 //        stage.show();
-    }
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
+	}
+	
+	@FXML
+	protected void onHelloButtonClick(){
+		welcomeText.setText("Welcome to JavaFX Application!");
+	}
+	
 }
 
